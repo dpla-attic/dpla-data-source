7c8bd63c02a9eabb6d8d88a09fbcf0a8d5ec5efe
Michael Della Bitta <mdellabitta@gmail.com>
2018-05-04 13:40:12 -0400

    changed to native aws java sdk for portability

--- build.sbt
+++ build.sbt
@@ -1,7 +1,4 @@
-lazy val commonSettings = Seq(
-    test in assembly := {},
-    mainClass in assembly := Some("dpla.data_source")
-)
+
 
 lazy val root = (project in file("."))
 //  .configs(IntegrationTest)
@@ -9,12 +6,9 @@ lazy val root = (project in file("."))
     name := "dpla-data-source",
     version := "0.1.0",
     scalaVersion := "2.11.8",
-    resolvers += "micronautics/scala on bintray" at "http://dl.bintray.com/micronautics/scala",
     libraryDependencies ++= Seq(
-      "org.json4s" %% "json4s-core" % "3.2.11",
-      "org.json4s" %% "json4s-jackson" % "3.2.11",
-      "org.apache.spark" %% "spark-core" % "2.1.0" % "provided" exclude("org.scalatest", "scalatest_2.11"),
+      "org.apache.spark" %% "spark-core" % "2.1.0" % "provided",
       "org.apache.spark" %% "spark-sql" % "2.1.0" % "provided" exclude("org.scalatest", "scalatest_2.11"),
-      "com.micronautics" %% "awslib_scala" % "1.1.12"
+      "com.amazonaws" % "aws-java-sdk" % "1.7.4" % "provided"
     )
 )
--- src/main/scala/dpla/data_source/DefaultSource.scala
+++ src/main/scala/dpla/data_source/DefaultSource.scala
@@ -1,11 +1,14 @@
 package dpla.data_source
 
-import com.micronautics.aws.S3
+import com.amazonaws.services.s3.model.ObjectListing
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3Client}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.sources._
 import org.apache.spark.sql.types._
 
+import scala.collection.JavaConversions._
+import scala.annotation.tailrec
 import scala.util.Try
 
 class DefaultSource extends RelationProvider with DataSourceRegister {
@@ -35,8 +38,8 @@ class DplaJsonlRelation(parameters: Map[String, String])(@transient val sqlConte
   override def buildScan(): RDD[Row] = data.rdd
 
   def getFiles: Seq[String] = {
-    val s3 = new S3()
-    val allJsonFiles = s3.listObjectsByPrefix("dpla-master-dataset", "", ".json").sorted
+    val s3 = new AmazonS3Client()
+    val allJsonFiles = listJsonFiles(s3, Nil, None)
 
     // providers: Unique provider prefixes.  Use the list given in the execution arguments, if present.
     val providers: Set[String] = parameters.get("providers") match {
@@ -45,25 +48,46 @@ class DplaJsonlRelation(parameters: Map[String, String])(@transient val sqlConte
     }
 
     // Find the most recent JSON files, looking at the last timestamp per provider
-    val tsPat: scala.util.matching.Regex = """^.*jsonl/(\d{8}_\d{6}).*""".r
+    val tsPat: scala.util.matching.Regex =
+      """^.*jsonl/(\d{8}_\d{6}).*""".r
 
     val recents = providers.flatMap(p => Try({
-        val pFiles = allJsonFiles.filter(_.startsWith(p + "/")).sorted
-        val tstamps = pFiles.map(f => {
-          val tsPat(ts: String) = f; ts
-        }).toSet // unique timestamp per provider
-        val mostRecentTs = parameters.get("maxTimestamp") match {
-          case Some(x) =>
-            val older = tstamps.toList.filter(ts => ts < x)
-            if (older.isEmpty) "NOTFOUND" else older.max
-          case None => tstamps.toList.max
-        }
-        pFiles.
-          filter(_.matches(s".*$mostRecentTs.*")).
-          map(f => s"s3a://dpla-master-dataset/$f")
-      }).getOrElse(List.empty))
+      val pFiles = allJsonFiles.filter(_.startsWith(p + "/")).sorted
+      val tstamps = pFiles.map(f => {
+        val tsPat(ts: String) = f;
+        ts
+      }).toSet // unique timestamp per provider
+      val mostRecentTs = parameters.get("maxTimestamp") match {
+        case Some(x) =>
+          val older = tstamps.toList.filter(ts => ts < x)
+          if (older.isEmpty) "NOTFOUND" else older.max
+        case None => tstamps.toList.max
+      }
+      pFiles.
+        filter(_.matches(s".*$mostRecentTs.*")).
+        map(f => s"s3a://dpla-master-dataset/$f")
+    }).getOrElse(List.empty))
 
     recents.toSeq
+  }
+
+  @tailrec
+  private final def listJsonFiles(s3: AmazonS3, acc: List[String], prev: Option[ObjectListing]): List[String] = {
+
+    val objectListing = prev match {
+      case None => s3.listObjects("dpla-master-dataset")
+      case Some(prevObjectListing) => s3.listNextBatchOfObjects(prevObjectListing)
+    }
+
+    val keys = for {
+      summary <- objectListing.getObjectSummaries.toList
+      key = summary.getKey
+      if key.endsWith(".json")
+    } yield key
+
+    val newAcc = acc ++ keys
+
+    if (objectListing.isTruncated) listJsonFiles(s3, newAcc, Some(objectListing)) else newAcc
   }
 
 }
